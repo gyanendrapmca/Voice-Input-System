99df5ab37c260d6e6ab93b5af7062d6f00e66469
gyanendrapmca <gyanendrap711@gmail.com>
2019-11-02 10:56:09 +0530

    Voice Input System

---
+++ VoiceInputSystem/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace VoiceInputSystem
+{
+    partial class vcInputSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(vcInputSystem));
+            this.enbleBtn = new System.Windows.Forms.Button();
+            this.dsbleBtn = new System.Windows.Forms.Button();
+            this.viewCmd = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.listCmd = new System.Windows.Forms.ListBox();
+            this.hideAllCmd = new System.Windows.Forms.Button();
+            this.time = new System.Windows.Forms.TextBox();
+            this.msg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // enbleBtn
+            // 
+            this.enbleBtn.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold);
+            this.enbleBtn.Location = new System.Drawing.Point(12, 12);
+            this.enbleBtn.Name = "enbleBtn";
+            this.enbleBtn.Size = new System.Drawing.Size(234, 56);
+            this.enbleBtn.TabIndex = 0;
+            this.enbleBtn.Text = "ENABLE VOICE";
+            this.enbleBtn.UseVisualStyleBackColor = true;
+            this.enbleBtn.Click += new System.EventHandler(this.enbleBtn_Click);
+            // 
+            // dsbleBtn
+            // 
+            this.dsbleBtn.Enabled = false;
+            this.dsbleBtn.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold);
+            this.dsbleBtn.Location = new System.Drawing.Point(283, 12);
+            this.dsbleBtn.Name = "dsbleBtn";
+            this.dsbleBtn.Size = new System.Drawing.Size(234, 56);
+            this.dsbleBtn.TabIndex = 1;
+            this.dsbleBtn.Text = "DISABLE VOICE";
+            this.dsbleBtn.UseVisualStyleBackColor = true;
+            this.dsbleBtn.Click += new System.EventHandler(this.dsbleBtn_Click);
+            // 
+            // viewCmd
+            // 
+            this.viewCmd.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewCmd.Location = new System.Drawing.Point(554, 12);
+            this.viewCmd.Name = "viewCmd";
+            this.viewCmd.Size = new System.Drawing.Size(234, 56);
+            this.viewCmd.TabIndex = 2;
+            this.viewCmd.Text = "VIEW ALL COMMANDS";
+            this.viewCmd.UseVisualStyleBackColor = true;
+            this.viewCmd.Click += new System.EventHandler(this.viewCmd_Click);
+            // 
+            // output
+            // 
+            this.output.Enabled = false;
+            this.output.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(12, 340);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(776, 88);
+            this.output.TabIndex = 3;
+            this.output.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.output.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listCmd
+            // 
+            this.listCmd.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listCmd.FormattingEnabled = true;
+            this.listCmd.HorizontalExtent = 1;
+            this.listCmd.ItemHeight = 15;
+            this.listCmd.Items.AddRange(new object[] {
+            "Hello",
+            "Word",
+            "Excel",
+            "Powerpoint",
+            "Close",
+            "Notepad",
+            "Wordpad",
+            "Calculator",
+            "Command",
+            "Paint",
+            "View",
+            "Date",
+            "Time",
+            "Google"});
+            this.listCmd.Location = new System.Drawing.Point(283, 83);
+            this.listCmd.Name = "listCmd";
+            this.listCmd.Size = new System.Drawing.Size(234, 244);
+            this.listCmd.TabIndex = 4;
+            this.listCmd.Visible = false;
+            // 
+            // hideAllCmd
+            // 
+            this.hideAllCmd.Enabled = false;
+            this.hideAllCmd.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hideAllCmd.Location = new System.Drawing.Point(12, 83);
+            this.hideAllCmd.Name = "hideAllCmd";
+            this.hideAllCmd.Size = new System.Drawing.Size(234, 56);
+            this.hideAllCmd.TabIndex = 5;
+            this.hideAllCmd.Text = "HIDE ALL COMMANDS";
+            this.hideAllCmd.UseVisualStyleBackColor = true;
+            this.hideAllCmd.Click += new System.EventHandler(this.hideAllCmd_Click);
+            // 
+            // time
+            // 
+            this.time.Enabled = false;
+            this.time.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.time.Location = new System.Drawing.Point(12, 216);
+            this.time.Multiline = true;
+            this.time.Name = "time";
+            this.time.Size = new System.Drawing.Size(233, 111);
+            this.time.TabIndex = 6;
+            // 
+            // msg
+            // 
+            this.msg.Enabled = false;
+            this.msg.Font = new System.Drawing.Font("Times New Roman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msg.Location = new System.Drawing.Point(554, 83);
+            this.msg.Multiline = true;
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(234, 244);
+            this.msg.TabIndex = 7;
+            this.msg.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // vcInputSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 427);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.time);
+            this.Controls.Add(this.hideAllCmd);
+            this.Controls.Add(this.listCmd);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.viewCmd);
+            this.Controls.Add(this.dsbleBtn);
+            this.Controls.Add(this.enbleBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "vcInputSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VOICE INPUT SYSTEM";
+            this.Load += new System.EventHandler(this.vcInputSystem_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button enbleBtn;
+        private System.Windows.Forms.Button dsbleBtn;
+        private System.Windows.Forms.Button viewCmd;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.ListBox listCmd;
+        private System.Windows.Forms.Button hideAllCmd;
+        private System.Windows.Forms.TextBox time;
+        private System.Windows.Forms.TextBox msg;
+    }
+}
+
---
+++ VoiceInputSystem/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+
+namespace VoiceInputSystem
+{
+    public partial class vcInputSystem : Form
+    {
+        //Initialize a new instance of the Speech Synthesizer
+        SpeechSynthesizer txtToSpeech = new SpeechSynthesizer();
+
+        //Initialize a new instance of the Speech Recognition
+        SpeechRecognitionEngine spToText = new SpeechRecognitionEngine(
+            new System.Globalization.CultureInfo("en-US"));
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetParent(IntPtr hwc, IntPtr hwp);
+
+        public vcInputSystem()
+        {
+            InitializeComponent();
+        }
+
+        private void vcInputSystem_Load(object sender, EventArgs e)
+        {
+            txtToSpeech.SelectVoiceByHints(VoiceGender.Female,VoiceAge.Child);
+            output.Text = "Welcome To Voice Input System";
+            Choices ch = new Choices();
+            ch.Add(new string[] {"Hello","Excel","Close","Word","Powerpoint","Notepad","Wordpad","Paint","Command","Calculator","Date","Time","Google","View","Hide" });
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Append(ch);
+            Grammar grammar = new Grammar(gb);
+            spToText.LoadGrammarAsync(grammar);
+            spToText.SetInputToDefaultAudioDevice();
+            spToText.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sE_SpeechRecognized);
+        }
+
+        void sE_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            //configure the audio output
+            txtToSpeech.SetOutputToDefaultAudioDevice();
+
+            if (e.Result.Text == "Hello")
+            {
+                //Change the content of Text
+                msg.Text = "Hello, I am Voice Input System! Developed By Gyanendra Pandey. Version V.1";
+
+                //Speak a String
+                txtToSpeech.Speak("Hello, I am Voice Input System! Developed By Gyanendra Pandey. Version V.1");
+            }
+
+            else if (e.Result.Text == "Close")
+            {
+                //Change the content of Text
+                msg.Text = "Closing Application";
+
+                //Speak a String
+                txtToSpeech.Speak("Closing Application");
+
+                //Command to Exit the Application
+                Application.Exit();
+            }
+
+            else if(e.Result.Text == "Excel")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Micro Soft Excel";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Micro Soft Excel");
+
+                //run the excel
+                run_excel();
+            }
+            else if (e.Result.Text == "Word")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Micro Soft Word";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Micro Soft Word");
+
+                //run the excel
+                run_word();
+            }
+
+            else if (e.Result.Text == "Powerpoint")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Micro Soft Powerpoint";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Micro Soft Powerpoint");
+
+                //run the excel
+                run_powerpnt();
+            }
+
+            else if (e.Result.Text == "Notepad")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Windows Notepad";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Windows Notepad");
+
+                //run the excel
+                run_notepad();
+            }
+
+            else if (e.Result.Text == "Wordpad")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Windows Wordpad";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Windows Wordpad");
+
+                //run the excel
+                run_wordpad();
+            }
+
+            else if (e.Result.Text == "Paint")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Windows Paint";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Windows Paint");
+
+                //run the excel
+                run_paint();
+            }
+
+            else if (e.Result.Text == "Command")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Windows Command Prompt";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Windows Command Prompt");
+
+                //run the excel
+                run_command();
+            }
+
+            else if (e.Result.Text == "Calculator")
+            {
+                //Change the content of Text
+                msg.Text = "Opening Windows Calculator";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Windows Calculator");
+
+                //run the excel
+                run_calculator();
+            }
+
+            else if(e.Result.Text == "Date")
+            {
+                output.Text = "Today Date is: " + DateTime.Now.ToString("yyyy-MM-dd");
+                txtToSpeech.Speak("Today Date is: " + DateTime.Now.ToString("yyyy-MM-dd"));
+            }
+
+            else if (e.Result.Text == "Time")
+            {
+                time.Text = "Current Time is: " + DateTime.Now.ToString("hh:mm:ss");
+                txtToSpeech.Speak("Current Time is: " + DateTime.Now.ToString("hh:mm:ss"));
+            }
+
+            else if (e.Result.Text == "View")
+            {
+                //Change the content of Message Text
+                msg.Text = "Displaying All Commands";
+                txtToSpeech.Speak("Displaying All Commands");
+                listCmd.Visible = true;
+                hideAllCmd.Enabled = true;
+            }
+
+            else if (e.Result.Text == "Hide")
+            {
+                //Change the content of Message Text
+                msg.Text = "Hiding All Commands";
+                txtToSpeech.Speak("Hiding All Commands");
+                listCmd.Visible = false;
+                hideAllCmd.Enabled = false;
+            }
+
+            else if (e.Result.Text == "Google")
+            {
+                //Change the content of Text
+                output.Text = "Opening Google Chrome";
+
+                //Speak a String
+                txtToSpeech.Speak("Opening Google Chrome");
+
+                //run the excel
+                run_chrome();
+            }
+            
+        }
+
+        private void enbleBtn_Click(object sender, EventArgs e)
+        {
+            spToText.RecognizeAsync(RecognizeMode.Multiple);
+            dsbleBtn.Enabled = true;
+            viewCmd.Enabled = false;
+            enbleBtn.Enabled = false;
+
+            //configure the audio output
+            txtToSpeech.SetOutputToDefaultAudioDevice();
+
+            msg.Text = "Enabling Voice Input System";
+            //Speak a String
+            txtToSpeech.Speak("Enabling Voice Input System");
+
+            output.Text = "Welcome To Voice Input System";
+            //Speak a String
+            txtToSpeech.Speak("Welcome To Voice Input System");
+            
+
+            output.Text = "Today Date is: " + DateTime.Now.ToString("yyyy-MM-dd");
+            //Speak a String
+            txtToSpeech.Speak("Today Date is: " + DateTime.Now.ToString("yyyy-MM-dd"));
+            
+            
+            time.Text = "Current Time is: " + DateTime.Now.ToString("hh:mm:ss");
+            //Speak a String
+            txtToSpeech.Speak("Current Time is: " + DateTime.Now.ToString("hh:mm:ss"));
+        }
+
+        private void dsbleBtn_Click(object sender, EventArgs e)
+        {
+            //configure the audio output
+            txtToSpeech.SetOutputToDefaultAudioDevice();
+
+            msg.Text = "Disable Voice Recognition";
+            //Speak a String
+            txtToSpeech.Speak("Disable Voice Recognition");
+
+            spToText.RecognizeAsyncStop();
+            dsbleBtn.Enabled = false;
+            enbleBtn.Enabled = true;
+            viewCmd.Enabled = true;
+        }
+
+        //1. View All Command List
+        private void viewCmd_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Listing All Command");
+            listCmd.Visible = true;
+            viewCmd.Enabled = false;
+            hideAllCmd.Enabled = true;
+        }
+
+        //2. Function To Run Microsoft Excel
+        public void run_excel()
+        {
+            try
+            { 
+                Process p = Process.Start("excel.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Exception MS Excel: "+e);
+            }
+        }
+
+        //3. Function To Run Microsoft Word
+        public void run_word()
+        {
+            try
+            {
+                Process p = Process.Start("winword.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception MS Word: " + e);
+            }
+        }
+
+        //4. Function To Run Windows Calculator
+        public void run_calculator()
+        {
+            try
+            {
+                Process p = Process.Start("calc.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception Calculator: " + e);
+            }
+        }
+
+        //5. Function To Windows Paint
+        public void run_paint()
+        {
+            try
+            {
+                Process p = Process.Start("mspaint.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Exception Windows Paint: " + e);
+            }
+        }
+
+        //6. Function To Run Command Prompt
+        public void run_command()
+        {
+            try { 
+            Process p = Process.Start("cmd.exe");
+            Thread.Sleep(500);
+            p.WaitForInputIdle();
+            SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Exception Command Prompt: " + e);
+            }
+        }
+
+        //7. Function To Run Google
+        public void run_chrome()
+        {
+            try
+            {
+                Process p = Process.Start("chrome.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Exception Google: "+e);
+            }
+        }
+        
+        //8. Function To Run Windows Notepad
+        public void run_notepad()
+        {
+            try
+            {
+                Process p = Process.Start("notepad.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception Notepad:" + e);
+            }
+        }
+
+        //9. Function To Run Microsoft Power Point
+        public void run_powerpnt()
+        {
+            try
+            {
+                Process p = Process.Start("powerpnt.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception MS Powerpoint: " + e);
+            }
+        }
+
+        //10. Function To Hide All Command
+        private void hideAllCmd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Hiding All Command");
+                listCmd.Visible = false;
+                viewCmd.Enabled = true;
+                hideAllCmd.Enabled = false;
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("Exception Hide Button: " + f);
+            }
+        }
+
+        //11. Function To Run Windows Notepad
+        public void run_wordpad()
+        {
+            try
+            {
+                Process p = Process.Start("wordpad.exe");
+                Thread.Sleep(500);
+                p.WaitForInputIdle();
+                SetParent(p.MainWindowHandle, this.Handle);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception Wordpad: " + e);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
